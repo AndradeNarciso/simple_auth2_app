fef362c85ce77de2631890a482753d57515ebefd
AndradeNarciso <andradegregorio277@gmail.com>
2026-01-28 13:25:29 +0200

    set up app

---
+++ src/main/java/com/andrade/simple_auth2_app/SimpleAuth2AppApplication.java
@@ -0,0 +1,13 @@
+package com.andrade.simple_auth2_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleAuth2AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleAuth2AppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andrade/simple_auth2_app/SimpleAuth2AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrade.simple_auth2_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleAuth2AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
