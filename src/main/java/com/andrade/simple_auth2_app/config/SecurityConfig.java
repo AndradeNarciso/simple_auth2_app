1c0d315dc5f53b0a9929a5d12d3b0db96969ae04
AndradeNarciso <andradegregorio277@gmail.com>
2026-02-02 14:05:42 +0200

    Encoder done

--- src/main/java/com/andrade/simple_auth2_app/config/SecurityConfig.java
+++ src/main/java/com/andrade/simple_auth2_app/config/SecurityConfig.java
@@ -11,42 +11,48 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-
     @Value("${jwt.prv.key}")
-    private  RSAPrivateKey privateKey;
+    private RSAPrivateKey privateKey;
 
     @Value("${jwt.pub.key}")
-    private  RSAPublicKey publicKey;
+    private RSAPublicKey publicKey;
 
     @Bean
     public SecurityFilterChain filter(HttpSecurity http) throws Exception {
 
-        
         return http.csrf(csrf -> csrf.disable()) // test env
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                //.authorizeHttpRequests(auth -> auth
+                // .authorizeHttpRequests(auth -> auth
 
-               // )
+                // )
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
     }
 
+    @Bean
+    public JwtEncoder encode() {
 
-    @Bean 
-    pulic JwtEncoder encode(){
-        return 
+        JWK jwk = new RSAKey.Builder(this.publicKey).privateKey(this.privateKey).build();
+        var jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+        return new NimbusJwtEncoder(jwks);
     }
 
-
-    @Bean JwtDecoder decoder (){
+    @Bean
+    JwtDecoder decoder() {
         return NimbusJwtDecoder.withPublicKey(publicKey).build();
     }
 
